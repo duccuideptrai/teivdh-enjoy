3cb06d3b93f300b2992d643722c0180215f52fc2
Hoang Minh Duc <duc.hm@aris-vn.com>
2015-05-28 13:01:42 +0700

    show watched movie info on index page

--- php-decode-server/index.php
+++ php-decode-server/index.php
@@ -1,20 +1,99 @@
+<?php
+require_once("config.php");
+define('START_OFFSET', 0);
+define('FILM_PER_PAGE', 100);
+
+function getLastestWatched($index,$pageSize,$con_db){
+	$sql="select distinct `xml_id`,`name`,`description`,`image_url` FROM `log` ORDER BY `create_time` DESC limit :pageSize OFFSET :index";
+	$stm=$con_db->prepare($sql);
+	$stm->bindValue(':index', (int) $index, PDO::PARAM_INT); 
+	$stm->bindValue(':pageSize', (int) $pageSize, PDO::PARAM_INT); 
+	$stm->execute();
+	return $stm->fetchAll();
+}
+
+function getMostWatched($index,$pageSize,$con_db){
+	$sql=<<<DUCCUI
+		select 
+			`xml_id`,`name`,`image_url`,count(*) as watch_count,`description`
+		FROM 
+			(
+		        SELECT  
+		        	`xml_id`,`name`,`description`,`image_url`
+		        FROM 
+		        	`log`
+		        GROUP BY 
+		        	`xml_id`, `ip`
+		    ) as tmp_filter
+		GROUP BY `xml_id`
+		ORDER BY  watch_count DESC limit :pageSize offset :index
+DUCCUI;
+
+	$stm=$con_db->prepare($sql);
+	$stm->bindValue(':index', (int) $index, PDO::PARAM_INT); 
+	$stm->bindValue(':pageSize', (int) $pageSize, PDO::PARAM_INT); 
+	$stm->execute();
+	return $stm->fetchAll();
+}
+
+
+try {
+	$con_db=new PDO($connect_string,DB_USER,DB_PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+} catch (Exception $e) {
+	die('khong mo dc database'. $e->getMessage());
+}
+
+$lastWatched=getLastestWatched(START_OFFSET,FILM_PER_PAGE,$con_db);
+$mostWatched=getMostWatched(START_OFFSET,FILM_PER_PAGE,$con_db);
+
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-	<title>My home page</title>
-	<script type="text/javascript">
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-41124233-2', 'auto');
-	  ga('send', 'pageview');
-	  ga('send','event','Web','view','index');
-	</script>
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/style.css" />
+	<title>Watched History</title>
 </head>
 <body>
-	<h1>My Page is on construction</h1>
+	<div class="page-content">
+		<div class="film-info">
+			<div class="film-info__title">Last Watch Video</div>
+			<div class="film-list">
+				<?php
+					foreach ($lastWatched as $film) {
+						?>
+						<div class="film-list__item">
+							<div class="film-list__item-wrapper">
+								<img class="film-list__item-thumbnail" src="<?=$film['image_url']?>" alt="<?=$film['name']?>" class="film-list__thumbnail" />
+								<div class="film-list__item-title"><?=$film['name']?></div>
+								<div class="film-list__item-description"><?=$film['description']?></div>
+							</div>
+						</div>
+						<?php
+					}
+				?>	
+			</div>
+		</div>
+		<div class="film-info">
+			<div class="film-info__title">Most Watch Video</div>
+			<div class="film-list">
+				<?php
+					foreach ($mostWatched as $film) {
+						?>
+						<div class="film-list__item">
+							<div class="film-list__item-wrapper">
+								<img class="film-list__item-thumbnail" src="<?=$film['image_url']?>" alt="<?=$film['name']?>" class="film-list__thumbnail" />
+								<div class="film-list__item-title"><?=$film['name']?></div>
+								<div class="film-list__item-description"><?=$film['description']?></div>
+							</div>
+						</div>
+						<?php
+					}
+				?>	
+			</div>
+		</div>
+	</div>
 </body>
 </html>
